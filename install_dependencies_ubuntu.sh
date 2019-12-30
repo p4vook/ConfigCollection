dd7b37e8fe21c7c934124615d60d0cc2aadcf0ef
pavel-the-best <paul.kalug@gmail.com>
2019-12-30 20:22:52 +0300

    Modified ubuntu dependencies

--- install_dependencies_ubuntu.sh
+++ install_dependencies_ubuntu.sh
@@ -1,1 +1,1 @@
-sudo apt install -y libncurses5-dev libatk1.0-dev libbonoboui2-dev libcairo2-dev git clang python3 python2 libpython3-dev glibc gcc make autoconf automake libtool gettext
+sudo apt install -y libncurses5-dev libatk1.0-dev libbonoboui2-dev libcairo2-dev git clang python3 python libpython3-dev libc gcc make autoconf automake libtool gettext
